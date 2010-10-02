eac816f99a42475ab472a8719cb8447bbf13de22
HIROKI <hirocast@gmail.com>
2010-10-03 01:20:44 +0900

    case 3

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -3,6 +3,9 @@ class fizzbuzz
 {
   public function say($number)
   {
+    if($number === 3){
+      return 'Fizz';
+    }
     return $number;
 
   }
--- fizzbuzzTest.php
+++ fizzbuzzTest.php
@@ -2,9 +2,10 @@
 require_once 'fizzbuzz.php';
 require_once 'lime.php';
 
-$test = new lime_test(2,new lime_output_color(true));
+$test = new lime_test(null,new lime_output_color(true));
 
 $fizzbuzz = new fizzbuzz();
 $test->is($fizzbuzz->say(2), 2,'2のとき2');
 $test->is($fizzbuzz->say(1), 1, '1のとき1');
+$test->is($fizzbuzz->say(3), 'Fizz', '3のときFizz');
 ?>
