767234f470bad204d417716d5a0b565eade48721
HIROKI <hirocast@gmail.com>
2010-10-03 01:17:36 +0900

    return number

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -3,11 +3,8 @@ class fizzbuzz
 {
   public function say($number)
   {
-    if($number === 1){
-      return 1;
-    } elseif($number === 2){
-      return 2;
-    }
+    return $number;
+
   }
 }
 ?>
